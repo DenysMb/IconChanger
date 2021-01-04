bedad6f63d2fce3eeee931eb4a8e6df2e4f59e59
Denys Madureira <denysmb@zoho.com>
2021-01-04 11:33:49 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/IconChooser.py
+mkdir -p ~/.local/share/nautilus/scripts
+mv IconChooser.py ~/.local/share/nautilus/scripts
