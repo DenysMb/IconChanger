e8afa9e6b89eeb147b80ec7bae3faac24365c714
Denys Madureira <denysmb@zoho.com>
2021-01-04 14:36:35 -0300

    Install.sh final

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 mkdir -p ~/.local/share/nautilus/scripts
 FILE=./IconChooser.py
 if ! [ -f "$FILE" ]; then
-	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/IconChooser.py -P /tmp/
-	mv /tmp/IconChooser.py ~/.local/share/nautilus/scripts
+	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/IconChooser.py -P ~/.local/share/nautilus/scripts
 else
 	mv IconChooser.py ~/.local/share/nautilus/scripts
 fi
