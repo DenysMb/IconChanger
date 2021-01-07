d87c43e91278ec6b2dad2dc4f9756ba41e795ded
Denys Madureira <denysmb@zoho.com>
2021-01-07 20:27:02 -0300

    Changes in install script

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 mkdir -p ~/.local/share/nautilus/scripts
 PLACES_FILE=./PlacesIconChooser.py
 APPS_FILE=./AppsIconChooser.py
-if ! [ -f "$FILE" ]; then
+if ! [ -f "$PLACES_FILE" ]; then
 	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/PlacesIconChooser.py -P ~/.local/share/nautilus/scripts
-	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/AppsIconChooser.py -P ~/.local/share/nautilus/scripts
 else
 	mv PlacesIconChooser.py ~/.local/share/nautilus/scripts
+fi
+if ! [ -f "$APPS_FILE" ]; then
+	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/AppsIconChooser.py -P ~/.local/share/nautilus/scripts
+else
 	mv AppsIconChooser.py ~/.local/share/nautilus/scripts
 fi
 chmod +x ~/.local/share/nautilus/scripts/PlacesIconChooser.py
