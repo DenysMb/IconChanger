f20c82b7a5c26a7f78fe7bd7694b88dc0217ee82
Denys Madureira <denysmb@zoho.com>
2021-01-07 21:14:17 -0300

    Add arguments to install script

--- install.sh
+++ install.sh
@@ -1,16 +1,64 @@
 #!/bin/bash
 mkdir -p ~/.local/share/nautilus/scripts
+
 PLACES_FILE=./PlacesIconChooser.py
 APPS_FILE=./AppsIconChooser.py
-if ! [ -f "$PLACES_FILE" ]; then
-	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/PlacesIconChooser.py -P ~/.local/share/nautilus/scripts
+
+move () {
+	echo "MOVE"
+	rm ~/.local/share/nautilus/scripts/PlacesIconChooser.py
+    if ! [ -f "$PLACES_FILE" ]; then
+		wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/PlacesIconChooser.py -P ~/.local/share/nautilus/scripts
+	else
+		mv PlacesIconChooser.py ~/.local/share/nautilus/scripts
+	fi
+	rm ~/.local/share/nautilus/scripts/AppsIconChooser.py
+	if ! [ -f "$APPS_FILE" ]; then
+		wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/AppsIconChooser.py -P ~/.local/share/nautilus/scripts
+	else
+		mv AppsIconChooser.py ~/.local/share/nautilus/scripts
+	fi
+}
+
+link () {
+	echo "LINK"
+    if ! [ -f "$PLACES_FILE" ]; then
+		wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/PlacesIconChooser.py
+	fi
+	if ! [ -f "$APPS_FILE" ]; then
+		wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/AppsIconChooser.py
+	fi
+	rm ~/.local/share/nautilus/scripts/PlacesIconChooser.py
+	ln ./PlacesIconChooser.py ~/.local/share/nautilus/scripts/PlacesIconChooser.py
+	rm ~/.local/share/nautilus/scripts/AppsIconChooser.py
+	ln ./AppsIconChooser.py ~/.local/share/nautilus/scripts/AppsIconChooser.py
+}
+
+if [ $# = 0 ]; then
+	move
 else
-	mv PlacesIconChooser.py ~/.local/share/nautilus/scripts
+	while [[ $# -gt 0 ]]
+	do
+	key="$1"
+
+	case $key in
+		-l|--link)
+		link
+		shift
+		;;
+		-m|--move)
+		move
+		shift
+		;;
+		*)    # unknown option
+		echo "DEFAULT"
+		POSITIONAL+=("$1") # save it in an array for later
+		shift
+		;;
+	esac
+	done
+	set -- "${POSITIONAL[@]}" # restore positional parameters
 fi
-if ! [ -f "$APPS_FILE" ]; then
-	wget https://raw.githubusercontent.com/DenysMb/IconChanger/master/AppsIconChooser.py -P ~/.local/share/nautilus/scripts
-else
-	mv AppsIconChooser.py ~/.local/share/nautilus/scripts
-fi
+
 chmod +x ~/.local/share/nautilus/scripts/PlacesIconChooser.py
 chmod +x ~/.local/share/nautilus/scripts/AppsIconChooser.py
